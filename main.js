547e8d70cfdd1d38f2c5b56b75bcf55ec6b99cb7
Raja Laha <59465193+Raja-11sep@users.noreply.github.com>
2024-01-27 13:49:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Frontend Web Developer","Comeptitive Programmer","Data Science Enthusiast"],
+    typeSpeed:50,
+    backSpeed:50,
+    backDelay:100,
+    loop:true,
+} );
